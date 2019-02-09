5675fc8a164c3f73b46784b4068c6a69ca5c98b7
Jacek Kowalski <eviathos@gmail.com>
2019-02-09 18:38:38 +0100

    up

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module RailsEviathos
     config.active_record.default_timezone = :local
     config.time_zone = 'Warsaw'
     config.i18n.fallbacks =[:en]
+    config.i18n.default_locale = :pl
+    config.i18n.fallbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
