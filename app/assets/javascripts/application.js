1c6ac069a86d8a4274a1f48d50f70d0b229ce930
Jacek Kowalski <eviathos@gmail.com>
2019-02-10 18:52:18 +0100

    up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,18 +69,6 @@ dataConfirmModal.setDefaults({
 
 
 
-
-
-// picture validate
-$('#clan_picture').bind('change', function() {
-  var size_in_megabytes = this.files[0].size/1024/1024;
-  if (size_in_megabytes > 5) {
-    alert('Maksymalny rozmiar pliku może wynosić 5MB. Proszę wybierz mniejszy plik.');
-  }
-});
-
-//
-
 $(document).ready(function(e) {
 // navbar
 
