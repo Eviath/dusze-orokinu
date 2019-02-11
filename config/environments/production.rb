e6c1df355e763291e68bc3dd515dbb8ce5593281
Jacek Kowalski <eviathos@gmail.com>
2019-02-11 13:49:49 +0100

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Rails_Eviathos_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Dusze_Orokinu_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'dusze-orokinu.pl'
