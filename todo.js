9aff5df24e4d5ca50d20f2d475dd96058b9bc3ea
Jacek Kowalski <eviathos@gmail.com>
2019-02-11 10:54:16 +0100

    up

---
+++ todo.js
@@ -0,0 +1,13 @@
+
+
+
+// TODO APP
+//  Responsywność
+
+// TODO FEATURES
+//  Dodać tworzenie poradników przez użytkowników - Stworzyć dla nich rangę?
+//  Dodać tworzenie buildów
+//  Galeria dla użytkowników
+
+// FIXME
+//  Poprawić wygląd Newsów /edit/new
