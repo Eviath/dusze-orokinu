ed3aa7c94462f6680cb1ee1e7b1e00dbcde45f16
Jacek Kowalski <eviathos@gmail.com>
2019-02-14 17:42:08 +0100

    Update tinyMce.js

--- app/webpacker/javascripts/tinyMce.js
+++ app/webpacker/javascripts/tinyMce.js
@@ -1,46 +1,20 @@
 import tinymce from 'tinymce/tinymce';
+import 'tinymce/themes/silver/theme';
 import 'tinymce/plugins/table';
-import 'tinymce/plugins/preview';
-import 'tinymce/plugins/searchreplace';
-import 'tinymce/plugins/fullscreen';
-import 'tinymce/plugins/image';
-import 'tinymce/plugins/autolink';
-import 'tinymce/plugins/directionality';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/media';
-import 'tinymce/plugins/template';
-import 'tinymce/plugins/codesample';
-import 'tinymce/plugins/charmap';
-import 'tinymce/plugins/hr';
-import 'tinymce/plugins/anchor';
-import 'tinymce/plugins/toc';
-import 'tinymce/plugins/insertdatetime';
 import 'tinymce/plugins/lists';
-import 'tinymce/plugins/wordcount';
-import 'tinymce/plugins/advlist';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/imagetools';
-import 'tinymce/plugins/help';
-import 'tinymce/plugins/textpattern';
-import 'tinymce/plugins/textcolor';
-import "./langs/pl";
-import 'tinymce/themes/silver/theme.min.js';
-
+import "tinymce/skins/ui/oxide/skin.min.css";
+import "tinymce/skins/content/default/content.min.css";
 
 function tinyMce() {
-
-
     tinymce.init({
         selector: 'textarea.tinymce',
-        plugins: 'preview searchreplace autolink directionality fullscreen image link media template codesample table charmap hr anchor toc insertdatetime advlist lists wordcount imagetools textpattern help',
-        toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-        image_advtab: true,
-        image_caption: true,
-        language: 'pl'
+        // some other settings, like height, language,
+        // order of buttons on your toolbar etc.
+        plugins: [
+            'table', 'lists'
+        ],
+        skin: false
     });
-
-
 }
-
-
+// if you're using a language file, you can place its content here
 export { tinyMce };
