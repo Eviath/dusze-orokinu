435b5e24abd7765dd101861e84cb4180de271e27
Jacek <eviathos@gmail.com>
2019-02-18 01:11:51 +0100

    features

--- todo.js
+++ todo.js
@@ -8,6 +8,8 @@
 //  Dodać tworzenie poradników przez użytkowników - Stworzyć dla nich rangę?
 //  Dodać tworzenie buildów
 //  Galeria dla użytkowników
+// FEATURED -- TWITCH DISCORD
 
 // FIXME
 //  Poprawić wygląd Newsów /edit/new
+
