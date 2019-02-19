a6940e0b3dbf10e3a872d2e64c73b51c6f8dc7ca
Jacek <eviathos@gmail.com>
2019-02-19 23:18:11 +0100

    api-updated

--- app/webpacker/packs/components/warframe_cetus.jsx
+++ app/webpacker/packs/components/warframe_cetus.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import axios from 'axios';
 
 const API = 'https://api.warframestat.us/pc/cetusCycle';
 
@@ -13,30 +14,46 @@ export class WarframeCetus extends Component {
             cetusCycle: {},
             isLoading: false,
             error: null,
+            cetusPresentCycle: "",
         };
     }
 
-    componentDidMount() {
-        this.setState({ isLoading: true });
-        this.timer = setInterval(()=> this.getCetusCycle(), 10000)
+    async componentDidMount() {
+        this.setState({ isLoading: true});
+        await this.getCetusCycle();
+        this.timer = setInterval(()=> this.getCetusCycle(),  10000);
+        this.setCetusPresentCycle()
     }
 
     async getCetusCycle(){
-        fetch(API)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong ...');
-                }
-            })
-            .then(data => this.setState({ cetusCycle: data, isLoading: false  }))
-            .catch(error => this.setState({ error, isLoading: false }));
+        try {
+            const result = await axios.get(API);
+            this.setState({
+                cetusCycle: result.data,
+                isLoading: false
+            });
+            } catch (error) {
+                 this.setState({
+                 error,
+                 isLoading: false
+                 });
+            }
+    }
+
+    setCetusPresentCycle(){
+        {console.log(this.state.cetusCycle.isDay)}
+        if (this.state.cetusCycle.isDay) {
+            this.setState({ cetusPresentCycle: 'Jest dzień.'});
+        } else {
+            this.setState({ cetusPresentCycle: 'Jest noc.'});
+        }
     }
 
 
+
+
     render() {
-        const { cetusCycle, isLoading, error } = this.state;
+        const { cetusCycle, isLoading, error, cetusPresentCycle } = this.state;
 
         if (error) {
             return <p>{error.message}</p>;
@@ -46,15 +63,13 @@ export class WarframeCetus extends Component {
             return <p>Loading ...</p>;
         }
 
-
-
-
         return (
             <div className={'cetusCycle-container container'}>
                 <h2>Plains of Eidolon</h2>
                 <div className={'cetusCycle-box row'}>
                     <div className={'cetusCycle-header mx-auto mt-5'}>
-                        <p>Zmiana pory <Moment locale="pl" interval={1000}  to={cetusCycle.expiry}/> </p>
+                        {cetusPresentCycle}
+                        <p>Zmiana pory <Moment locale="pl" interval={1000} to={cetusCycle.expiry}/></p>
                         <p>(<Moment locale='pl' format='DD-MM-YY HH:mm' date={cetusCycle.expiry}/>)</p>
                     </div>
                 </div>
