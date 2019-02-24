0a21886083b9b2b2369c6c4122b5bdbf9861ed80
Jacek <eviathos@gmail.com>
2019-02-24 16:42:28 +0100

    update-timers
    Api changed to parser, code revamp, cetus, sortie, alerts update.

--- app/webpacker/packs/components/warframe_sortie.jsx
+++ app/webpacker/packs/components/warframe_sortie.jsx
@@ -8,6 +8,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 const API = 'http://content.warframe.com/dynamic/worldState.php';
 const WorldState = require('warframe-worldstate-parser');
 
+// WFCD API
 // const API = 'https://api.warframestat.us/pc/sortie';
 
 export class Sortie extends Component {
@@ -17,27 +18,52 @@ export class Sortie extends Component {
         super(props);
         this.state = {
             sortie: {},
+            sortieEta: null,
             isLoading: false,
             error: null,
             isHidden: [true, true, true]
         };
     }
 
+    showHide(num){
+        this.setState((prevState) => {
+            const newItems = [...prevState.isHidden];
+            newItems[num] = !newItems[num];
+            return {isHidden: newItems};
+        });
+    }
 
     async componentDidMount() {
         this.setState({isLoading: true});
+
         await this.getSortieStatus();
         this.timer = setInterval(() => this.getSortieStatus(), 60000);
     }
 
+    componentWillUnmount() {
+        this.timer = null;
+    }
+
     async getSortieStatus() {
+        // fetch api
         try {
             const result = await axios.get(API);
-            const ws = new WorldState(result);
+            const ws = new WorldState(JSON.stringify(result.data));
             this.setState({
                 sortie: ws.sortie,
                 isLoading: false
             });
+
+
+            // set state of ETA time sortie, refresh every second
+            this.interval = setInterval(() => {
+                this.setState({
+                    sortieEta: ws.sortie.getETAString(),
+                    isLoading: false
+                });
+            }, 1000);
+
+
         } catch (error) {
             this.setState({
                 error,
@@ -48,7 +74,49 @@ export class Sortie extends Component {
 
 
     render() {
-        const {sortie, isLoading, error} = this.state;
+        const {sortie, sortieEta, isLoading, error, isHidden} = this.state;
+
+        const variants = sortie.variants;
+
+        const sortieVariants = (
+
+            variants && variants.map((mission, key) =>
+                <div key={key} className="variants-container">
+                    {!isHidden[key] && (
+                        <div className="sortie-variant-header row text-center">
+                            <div className="col-lg-2 variant-item type">
+                                <p>Mission Type</p>
+                            </div>
+                            <div className="col-lg-4 variant-item node">
+                                <p>Planet & Node</p>
+                            </div>
+                            <div className="col-lg-5 variant-item modifier">
+                                <p>Modifier</p>
+                                <p>{mission.modifierDescription}</p>
+                            </div>
+                            <div className="col-lg-1 variant-item help">
+                            </div>
+                        </div>
+                    )}
+                    <div className={'row text-center sortie-variant-box'}>
+                        <div className="col-lg-2 variant-item type">
+                            <p>{mission.missionType}</p>
+                        </div>
+                        <div className="col-lg-4 variant-item node">
+                            <p>{mission.node}</p>
+                        </div>
+                        <div className="col-lg-5 variant-item modifier">
+                            <p>{mission.modifier}</p>
+                        </div>
+                        <div className="col-lg-1 variant-item help">
+                            <span  onClick={()=>this.showHide(key)}> <FontAwesomeIcon icon={faQuestionCircle} /> </span>
+                        </div>
+                    </div>
+                </div>
+
+            )
+        );
+
 
         if (error) {
             return <p>{error.message}</p>;
@@ -61,7 +129,20 @@ export class Sortie extends Component {
         return (
             <div className={'sortie-container'}>
                 <h2>Sortie</h2>
-                {console.log(sortie)}
+
+                <div className={'sortie-box row'}>
+                    <div className={'sortie-header mx-auto mt-5 pb-5'}>
+                        <h5>{sortie.boss}</h5>
+                        <p>{sortie.faction}</p>
+                        <p>{sortieEta}</p>
+                        <p>(<Moment locale='pl' format='DD-MM-YY HH:mm' date={sortie.expiry}/>)</p>
+                    </div>
+
+                    <div className="container sortie-variants">
+                        {sortieVariants}
+                    </div>
+
+                </div>
             </div>
         );
     }
