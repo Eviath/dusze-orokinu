fbf89304b344665c96db96a2e1781e7c37fe9607
Jacek <eviathos@gmail.com>
2019-02-24 19:00:15 +0100

    up

--- app/controllers/warframe_controller.rb
+++ app/controllers/warframe_controller.rb
@@ -1,4 +1,11 @@
 class WarframeController < ApplicationController
+  after_action :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
 
   def poe
   end
