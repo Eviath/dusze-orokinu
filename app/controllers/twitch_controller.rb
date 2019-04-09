592aea63fb5248cee7c0fb7cf57d817cad114fa1
Jacek <eviathos@gmail.com>
2019-04-09 14:34:24 +0200

    update

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -26,18 +26,25 @@ class TwitchController < ApplicationController
   protected
 
   def check_session
-    session['access_token'].nil?
+    if session['access_token_expiration_date'].nil? || Time.now > session['access_token_expiration_date']
+      logger.debug "Access token expired, fetching new access token."
+      request_access_token
+    else
+      logger.debug "Time now is #{Time.now}, Access token valid to #{session['access_token_expiration_date']}"
+    end
   end
 
   def request_access_token
-    @client_id = ENV['TWITCH_CLIENT_ID']
-    @secret_key = ENV['TWITCH_CLIENT_SECRET']
-    @redirect_uri = ''
+    logger.info "Request access token is run"
+    @client_id = ENV['TWITCH_CLIENT_ID'] || Rails.application.credentials.TWITCH_CLIENT_ID
+    @secret_key = ENV['TWITCH_CLIENT_SECRET'] || Rails.application.credentials.TWITCH_CLIENT_SECRET
     @url = "https://id.twitch.tv/oauth2/token?client_id=#{@client_id}&client_secret=#{@secret_key}&grant_type=client_credentials"
     @result = HTTParty.post(@url)
-
     # set access token in cookies
+    @expiry = @result['expires_in']
     session['access_token'] = @result['access_token']
+    session['access_token_expiration_date'] = Time.now + @expiry/1000.0
+    logger.info "Expiration date of access token #{session['access_token_expiration_date']}"
   end
 
 
